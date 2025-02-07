4b26464b0c9aa6080e0229e85f90d4fee5dc0725
Ardor <Ardor@DESKTOP-BQF7240>
2025-02-07 16:24:06 +0800

    Добавьте файлы проекта.

---
+++ Sorting numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ Дан массив чисел (минимум 10 чисел). Надо вывести в консоль числа отсортированы, от меньшего до большего.
+Нельзя использовать Array.Sort. Используйте пузырьковую сортировку.
+ */
+using System;
+using System.Text;
+
+namespace CSharplight
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            int elements = 15;
+            int minValue = 0;
+            int maxValue = 31;
+            int[] numbers = new int[elements];
+
+            Random random = new Random();
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue);
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        // Меняем местами
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", numbers));
+        }
+    }
+}
