cf3ed4ac67f2cf9280442e5e78e434b416724b8d
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2025-02-10 18:25:45 +0800

    Update Program.cs

--- Sorting numbers/Program.cs
+++ Sorting numbers/Program.cs
@@ -22,20 +22,20 @@ namespace CSharplight
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = random.Next(minValue, maxValue);
+                numbers[i] = random.Next(minValue, maxValue +1);
                 Console.Write(numbers[i] + " ");
             }
 
             Console.WriteLine();
 
-            int n = numbers.Length;
-            for (int i = 0; i < n - 1; i++)
+            int arrayLength = numbers.Length;
+
+            for (int i = 0; i < arrayLength - 1; i++)
             {
-                for (int j = 0; j < n - 1 - i; j++)
+                for (int j = 0; j < arrayLength - 1 - i; j++)
                 {
                     if (numbers[j] > numbers[j + 1])
                     {
-                        // Меняем местами
                         int temp = numbers[j];
                         numbers[j] = numbers[j + 1];
                         numbers[j + 1] = temp;
